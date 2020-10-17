7e5fabc44537a8c6a406ff634e042d746d54209f
logic-fabric <lowic@logic-fabric.fr>
2020-10-17 11:21:05 +0200

    test: add a test about the type of the paramter passed to collatz

--- chapter-3-functions/collatz.py
+++ chapter-3-functions/collatz.py
@@ -1,6 +1,6 @@
 """Implement a function to compute the sequence of the Collatz conjecture ("conjecture de Syracuse" in french):
 
-1. the sequence starts with a non null integer
+1. the sequence starts with a non null positive integer
 2. if a number N is even, its successor is N / 2
 3. else, its successor is 3 * N + 1
 
@@ -9,7 +9,12 @@ The conjecture says that this sequence will always end in the cycle (1, 4, 2).
 
 
 def collatz(n):
-    return 3 * n + 1 if n % 2 else n // 2
+    if type(n) != int or n < 1:
+        raise ValueError(
+            "The collatz function needs a non null positive integer."
+        )
+    else:
+        return 3 * n + 1 if n % 2 else n // 2
 
 
 if __name__ == "__main__":
--- chapter-3-functions/test_collatz.py
+++ chapter-3-functions/test_collatz.py
@@ -1,6 +1,26 @@
+import pytest
+
 from collatz import collatz
 
 
-def test_collatz():
+def test_collatz_compute_well():
     assert collatz(4) == 2
     assert collatz(1) == 4
+
+def test_collatz_raises_an_exception_without_a_non_null_positive_integer():
+    with pytest.raises(ValueError):
+        collatz(0)
+    with pytest.raises(ValueError):
+        collatz(-1)
+    with pytest.raises(ValueError):
+        collatz(True)
+    with pytest.raises(ValueError):
+        collatz(4.0)
+    with pytest.raises(ValueError):
+        collatz("NaN")
+    with pytest.raises(ValueError):
+        collatz([4])
+    with pytest.raises(ValueError):
+        collatz((4,))
+    with pytest.raises(ValueError):
+        collatz({4})
